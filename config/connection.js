5bcdd39e58df2a1ebeb4a9099eaed7481ddc2546
Paul Gottinger <pgottinger@outlook.com>
2022-03-24 00:09:58 -0500

    lesson 1.6

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,17 @@
 const Sequelize = require("sequelize");
 
-require("dotenv").config();
+// require("dotenv").config();
 
 // create connection to our db
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PW,
+  // process.env.DB_NAME,
+  // process.env.DB_USER,
+  // process.env.DB_PW,
   {
     host: "localhost",
+    user: "host",
+    password: "grandMA7grundy",
+    database: "just_tech_news_db",
     dialect: "mysql",
     port: 3306,
   }
